be2c0e5d2816d1cf296055b767a9be8842bb211d
addhiee <ahmadaffandi317@gmail.com>
2025-04-08 12:49:17 +0700

    absen test

---
+++ script.js
@@ -0,0 +1,33 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const uid = document.getElementById("uid").value;
+  const nama = document.getElementById("nama").value;
+  const keterangan = document.getElementById("keterangan").value;
+
+  const statusEl = document.getElementById("status");
+
+  fetch("https://script.google.com/macros/s/AKfycbybNYX-GPkOJ5lUB0N2PeUVRpVsca4RDpsdh0T8QlxAQH9YxTAi3-I5D_RE674idCA/exec", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      uid: uid,
+      nama: nama,
+      keterangan: keterangan,
+    }),
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      statusEl.innerHTML = `<span style="color: green;">${data}</span>`;
+      document.getElementById("absenForm").reset();
+    })
+    .catch(() => {
+      statusEl.innerHTML = "<span style='color: red;'>Gagal mengirim absen.</span>";
+    });
+
+  setTimeout(() => {
+    statusEl.innerHTML = "";
+  }, 5000);
+});
